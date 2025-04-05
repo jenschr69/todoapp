30e34c21977275d1e572cac064e7879be4b653c4
Jens Christensen - Glodexa <jens@glodexa.com>
2025-04-05 20:21:06 +0200

    Initial commit

--- README.md
+++ README.md
@@ -6,3 +6,9 @@ blank
 
 - **Database**: mongodb
 - **Storage Adapter**: localDisk
+
+Aaron Saunders YT video
+https://www.youtube.com/watch?v=v_ga0nzm-wU
+
+Repo
+https://github.com/aaronksaunders/my-payload-todo-1-2025
--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -15,45 +15,23 @@ export default async function HomePage() {
 
   const fileURL = `vscode://file/${fileURLToPath(import.meta.url)}`
 
+  const todos = await payload.find({
+    collection: 'todos',
+    limit: 100,
+  })
+
   return (
-    <div className="home">
-      <div className="content">
-        <picture>
-          <source srcSet="https://raw.githubusercontent.com/payloadcms/payload/main/packages/ui/src/assets/payload-favicon.svg" />
-          <Image
-            alt="Payload Logo"
-            height={65}
-            src="https://raw.githubusercontent.com/payloadcms/payload/main/packages/ui/src/assets/payload-favicon.svg"
-            width={65}
-          />
-        </picture>
-        {!user && <h1>Welcome to your new project.</h1>}
-        {user && <h1>Welcome back, {user.email}</h1>}
-        <div className="links">
-          <a
-            className="admin"
-            href={payloadConfig.routes.admin}
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            Go to admin panel
-          </a>
-          <a
-            className="docs"
-            href="https://payloadcms.com/docs"
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            Documentation
-          </a>
-        </div>
+    <div>
+      <h1>Payload To Do List {user?.email}</h1>
+      <div className="todos">
+        <h2>Todos</h2>
+        {todos.docs.map((todo) => (
+          <div key={todo.id}>
+            <h3>{todo.title}</h3>
+            <p>{todo.description}</p>
+          </div>
+        ))}
       </div>
-      <div className="footer">
-        <p>Update this page by editing</p>
-        <a className="codeLink" href={fileURL}>
-          <code>app/(frontend)/page.tsx</code>
-        </a>
       </div>
-    </div>
   )
 }
